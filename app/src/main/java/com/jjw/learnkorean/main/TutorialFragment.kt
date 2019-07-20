be8a03f8413c8a587962af9728d7cf5d1b698987
jjw <wkdwoduf333@naver.com>
2019-07-20 09:54:39 +0900

    유튜브 플레이어 라이브러리 추가

--- app/src/main/java/com/jjw/learnkorean/main/TutorialFragment.kt
+++ app/src/main/java/com/jjw/learnkorean/main/TutorialFragment.kt
@@ -1,19 +1,37 @@
 package com.jjw.learnkorean.main
 
-import android.media.MediaPlayer
+
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.*
 import com.jjw.learnkorean.R
 import kotlinx.android.synthetic.main.fragment_main_tutorial.*
 import android.util.Log
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerFragment
 
 
-class TutorialFragment : Fragment(), SurfaceHolder.Callback {
+class TutorialFragment : Fragment(), YouTubePlayer.OnInitializedListener {
 
-    private lateinit var surfaceView: SurfaceView
-    private lateinit var surfaceHolder: SurfaceHolder
-    private lateinit var mediaPlayer: MediaPlayer
+    private val videoID = "N76HNPfI4zs"
+
+    override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, youtubePlayer: YouTubePlayer?, isReady: Boolean) {
+        if (!isReady) {
+            val playKey = "AIzaSyCXrE4LYCRdSsX3OsdhMZPyok1EwsruKC8"
+            youtubePlayer!!.cueVideo(playKey)
+        }
+    }
+/*
+
+//    private lateinit var surfaceView: SurfaceView
+//    private lateinit var surfaceHolder: SurfaceHolder
+//    private lateinit var mediaPlayer: MediaPlayer
 
     override fun surfaceChanged(holder: SurfaceHolder?, format: Int, width: Int, height: Int) {
     }
@@ -52,6 +70,7 @@ class TutorialFragment : Fragment(), SurfaceHolder.Callback {
 
 
     }
+*/
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -61,8 +80,11 @@ class TutorialFragment : Fragment(), SurfaceHolder.Callback {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        surfaceHolder = surfaceView.holder
-        surfaceHolder.addCallback(this)
+//        surfaceHolder = surfaceView.holder
+//        surfaceHolder.addCallback(this)
+
+        youtube_view.initialize(videoID, this)
+
         //뷰 설정
         tv_fragment_main_tutorial.text = "fragment_main_tutorial"
     }
