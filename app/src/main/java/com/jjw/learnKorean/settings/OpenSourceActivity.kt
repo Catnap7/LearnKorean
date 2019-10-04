28a4d30142da94c729382de7ecc0b6c5b0dc4945
wkdwoduf333@Naver.com <a8949005>
2019-10-05 00:52:23 +0900

    번역작업..

---
+++ app/src/main/java/com/jjw/learnKorean/settings/OpenSourceActivity.kt
@@ -0,0 +1,13 @@
+package com.jjw.learnKorean.settings
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jjw.learnKorean.R
+
+class OpenSourceActivity :AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_opensource)
+    }
+}
