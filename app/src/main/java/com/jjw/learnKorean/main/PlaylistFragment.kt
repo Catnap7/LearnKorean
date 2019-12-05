5bdd4d0558805a93c050bf35fdc9df9db947cf49
wkdwoduf333@Naver.com <a8949005>
2019-12-05 17:06:39 +0900

    Firestore 에서 video 리스트 받아오도록 수정중

--- app/src/main/java/com/jjw/learnKorean/main/PlaylistFragment.kt
+++ app/src/main/java/com/jjw/learnKorean/main/PlaylistFragment.kt
@@ -6,21 +6,20 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.FirebaseFirestore
 import com.jjw.learnKorean.R
 import com.jjw.learnKorean.korean.KoreanContentsFilterActivity.Companion.TAG
 import com.jjw.learnKorean.playlist.PlaylistAdapter
-import kotlinx.android.synthetic.main.fragment_main_playlist.*
-import kotlinx.android.synthetic.main.fragment_main_playlist.view.*
 import kotlinx.android.synthetic.main.fragment_main_playlist.view.rv_youtubePlaylist
+
 
 class PlaylistFragment : androidx.fragment.app.Fragment(){
 
     private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
 
-    private val playList = arrayOf("N76HNPfI4zs","RoepaDKQ0PQ","wksUmcAx7z4","4lKqFR-67RI","3-FXW0CW_8o","gqvEO6o1Mx8","FsEGaURjZ8w","fgja5tdRB8o")
+//    private val playList = arrayOf("N76HNPfI4zs","RoepaDKQ0PQ","wksUmcAx7z4","4lKqFR-67RI","3-FXW0CW_8o","gqvEO6o1Mx8","FsEGaURjZ8w","fgja5tdRB8o")
+    private var playList = ArrayList<String>()
     private val playlist_title = arrayOf("[BANGTAN BOMB] JK's Heart (for ARMY♥︎) - BTS (방탄소년단)",
         "[BANGTAN BOMB] Arm wrestling! WHO IS THE WINNER?! - BTS (방탄소년단)","[BANGTAN BOMB] RM’s Surprise(?) Birthday Party - BTS (방탄소년단)","[BANGTAN BOMB] '풍경' Special LIVE - BTS (방탄소년단)",
         "[BANGTAN BOMB] V&Jungkook Singing at standby time - BTS (방탄소년단)","[BANGTAN BOMB] Let's Pizza Party! - BTS (방탄소년단)","[BANGTAN BOMB] The secret of BTS' beard - BTS (방탄소년단)",
@@ -36,23 +35,20 @@ class PlaylistFragment : androidx.fragment.app.Fragment(){
         rv_youtubePlaylist.isNestedScrollingEnabled = false
         rv_youtubePlaylist.layoutManager = LinearLayoutManager(activity)
 
-        playlistAdapter = PlaylistAdapter(activity!!,playList,playlist_title)
-
-        rv_youtubePlaylist.adapter = playlistAdapter
-
         getFirestoreVideos()
+
+        playlistAdapter = PlaylistAdapter(activity!!,playList,playlist_title)
+        rv_youtubePlaylist.adapter = playlistAdapter
 
         return playlistView
     }
 
     override fun onResume() {
         super.onResume()
-
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
     }
 
     override fun onStop() {
@@ -61,20 +57,42 @@ class PlaylistFragment : androidx.fragment.app.Fragment(){
 
     fun getFirestoreVideos(){
 
-        val docRefdb = db.collection("LearnKorean").document("Videos")
+        val docRefPlayList = db.collection("LearnKorean").document("Videos")
 
-        docRefdb.get().addOnSuccessListener { document ->
+        docRefPlayList.get().addOnSuccessListener { document ->
+            if (document != null) {
+                Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+
+                var size:Int = document.data!!.size()
+                for(i in size){
+
+                }
+                val dc = document.toObject(ArrayList::class.java)
+                playList.addAll(document.data.get(""))
+
+//                for (i in 0 until document.data.size()) {
+//                    val brandName = item.getJSONObject(i)
+//                    brandList.add(brandName.getString("name"))
+//                }
+//                val types = document.toObject(ArrayList::class.java)
+//                playList.addAll(types)
+            } else {
+                Log.d(TAG, "No such document")
+            }
+        }
+
+        val docRef = db.collection("LearnKorean").document("Videos").collection("N76HNPfI4zs").document("Subtitle")
+
+        docRef.get().addOnSuccessListener { document ->
                 if (document != null) {
                     Log.d(TAG, "DocumentSnapshot data: ${document.data}")
-                    Toast.makeText(activity, "DocumentSnapshot data: ${document.data}", Toast.LENGTH_SHORT).show()
                 } else {
                     Log.d(TAG, "No such document")
-                    Toast.makeText(activity, "No such document", Toast.LENGTH_SHORT).show()
                 }
             }
             .addOnFailureListener { exception ->
                 Log.d(TAG, "get failed with ", exception)
-                Toast.makeText(activity, "get failed with", Toast.LENGTH_SHORT).show()
             }
     }
+    companion object { var TAG = "PlaylistFragment" }
 }
