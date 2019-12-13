5ad8b2109eee886a4bf75a120c52db0bd66c61b1
wkdwoduf333@Naver.com <a8949005>
2019-12-13 16:37:17 +0900

    Http 통신 오류뜨는거 수정,오픈소스 텝 등등 수정함

--- app/src/main/java/com/jjw/learnKorean/common/Subtitles.kt
+++ app/src/main/java/com/jjw/learnKorean/common/Subtitles.kt
@@ -1,66 +1,6 @@
 package com.jjw.learnKorean.common
 
 class Subtitles {
-    //"N76HNPfI4zs","RoepaDKQ0PQ","wksUmcAx7z4","4lKqFR-67RI","3-FXW0CW_8o","gqvEO6o1Mx8","FsEGaURjZ8w","fgja5tdRB8o"
-
-    val N76HNPfI4zs = arrayOf("하트","하트","하트 하트으으~ ","휴지로 하트를 접고 싶은데 어떻게접는지 모르겠어요",
-       "여기서 이렇게한다음에","그렇지..","그렇지!","빨리 가야 돼요 우리?","아 잠시만 나 왜이래","하트",
-         "다 쪼개졌네 하트가 이럼 안되는데","실패")
-    //진정국 하트
-    val N76HNPfI4zs_time = arrayOf(5,10,15,23,28,32,34,47,51,54,65,66)
-    val N76HNPfI4zs_sub = arrayOf("Heart","Heart","Heart, heart~","I want to fold the heart with tissue, but I don't know how to fold it.","Here we go.at the sound of","That's right..","Yeah!","We have to go. We?","Oh, wait a minute. What's wrong with me?","Heart","It's all broken up. Hearts are not going to work","failure")
-    val N76HNPfI4zs_diction = arrayOf("hateu","hateu","hateu hateueueu~","hyujiro hateureul jeopgo sipeunde eotteohgejeopneunji moreugesseoyo","yeogiseo ireohgehandaeume","geureohji..","geureohji!!",
-                                    "ppalli gaya dwaeyo uri?","a jamsiman na waeirae","hateu","da jjogaejyeossne hateuga ireom andoeneunde","silpae")
-
-    //[BANGTAN BOMB] Arm wrestling! WHO IS THE WINNER?! - BTS (방탄소년단)
-    val RoepaDKQ0PQ = arrayOf("만덕동!","만덕동!!","만덕동!","아 만덕동가면 이거 꼭 들어줘야겠네","만덕동!","만덕이요? 제가 제 고향이요","아니 잠깐 팔꿈치가","팔꿈치는 반칙 아니여?!","아니 너무 반칙 ㅋㅋ","만덕동~","오오 센데","오오 센데~","만덕동~","오오~","자 이쪽뭐야 이쪽 무슨동이야?","와 무슨구? 굉장합니다","박빙이었어","맨덕동쓰!","맨덕동쓰!","만덕동!","내고향 만덕동","만덕동!")
-    val RoepaDKQ0PQ_time = arrayOf(3,5,13,16,18,22,34,37,39,41,46,48,59,61,62,68,70,71,72,76,77,80)
-    val RoepaDKQ0PQ_sub = arrayOf("Mandeok-dong!","Mandeok-dong!!","Mandeok-dong!","Oh, If I go to Mandeok-dong, I will listen to this","Mandeok-dong!","Manduk? My, My homeland","Wait, you use en elbow","Isn't elbow foul?!","No, it's too foul","Mandeok-dong~","Oh, you're strong","Oh, you're strong bro~","Mandeok-dong~","Oh~~","Come on, What's this side of the street?"
-                                    ,"Wow, What's this side of the street? It's amazing","it was neck and neck","Mandeok-dong!","Mandeok-dong!","my homeland manduk-dong","Mandeok-dong")
-    val RoepaDKQ0PQ_diction = arrayOf("mandeokdong!","mandeokdong!!","mandeokdong!","a mandeokdonggamyeon igeo kkok deureojwoyagessne","mandeokdong!","mandeogiyo? jega je gohyangiyo","ani jamkkan palkkumchiga","palkkumchineun banchik aniyeo?!","ani neomu banchik keukeu","mandeokdong~","oo sende","oo sende~","mandeokdong~","oo~"
-                                    ,"ja ijjokmwoya ijjok museundongiya?","wa museungu? goengjanghapnida","bakbingieosseo","maendeokdongsseu!","maendeokdongsseu!","mandeokdong!","naegohyang mandeokdong","mandeokdong!")
-
-    //RM 생일
-    val wksUmcAx7z4 = arrayOf("음 맛있어","으으음~~","남준이 오늘 생일입니다","남준 오늘 생일이야","아 지금 투어때 지민이랑 남준이가 생일이구나","형 특별하게 생일이 이틀이네요?","시간보면 한국 내일로 뜨잖아요","아 아 거기가 지금 일이니까","남준이 깜짝 생일파티 한번만 준비해주세요","남준아 어떤 케익 먹고싶냐?","반복된 치즈케익~","남준아 깜짝 생일파티 언제해줄까?","너가 원할때 해줄게","아악!!","너 생일축하노래 불러준다",
-                                "고맙다!","해피 벌스데이 투 준~~","해피벌스데이 ~","갑자기?","해피 벌스데이 남준쓰~","너무 고맙다 진짜","해피 벌스데이 마이 브로~~~","해피 벌스데이 투 유~~! ♬","화음 쩔었지?","우리 아무생각 안하고","생일빵!! 아악!!")
-    val wksUmcAx7z4_time = arrayOf(1,5,11,13,17,20,22,26,31,33,35,40,42,46,52,53,54,57,58,59,63,65,66,67,68,73,80,88,90)
-    val wksUmcAx7z4_sub = arrayOf("Mmm. Delicious","Mmm~~","Nam Jun is today's birthday","Nam Jun Today's Birthday","Oh, I see Ji Min and Nam Jun’s birthday on the tour","You have a special birthday for two days?","No, it will be tomorrow in Korea when you look at the time","Oh, that's the 12th of the day","Please prepare for Nam Jun's surprise birthday party", "What kind of cake do you want?",
-                                    "As expected, BT21 is good for cake","Haha, repeated cheesecake~","Nam Jun, when do you want to have a surprise birthday party?"," I'll do it when you want","Fans sing you a birthday song. Nam Jun","Huh?","Fans sing your birthday song","Hit when is someone's birthdayIt's like a present","Tnx!","Happy Birthday to June~~","Happy Birthday~"
-                                    ,"suddenly?","Nam Jun~","Thank you so much, really","Happy Birthday My Bro~~~","Happy Birthday to U~~!","It was a good chord, wasn't it?","We don't think about anything","Hit of Birthday!! Ouch!!")
-    val wksUmcAx7z4_diction = arrayOf("eum masisseo","eueueum~~","namjuni oneul saengiripnida","namjun oneul saengiriya","a jigeum tueottae jiminirang namjuniga saengiriguna","hyeong teukbyeolhage saengiri iteurineyo?","ani siganbomyeon hanguk naeillo tteujanhayo","a a geogiga jigeum 12irinikka","namjuni kkamjjak saengilpati hanbeonman junbihaejuseyo","namjuna eotteon keik meokgosipnya?"
-                                    ,"yeoksi keigeun BT21 iji","haha, banbokdoen chijeukeik~","namjuna kkamjjak saengilpati eonje haejulkka?","neoga wonhalttae haejulge","neo saengilchukhanorae bureunda ya namjuna","eung?","neo saengilchukhanorae bulleo","saengilppang!","gomapda!","haepi beolseudei tu jun~~","haepibeolseudei~","gapjagi?","namjunsseu~","neomu gomapda jinjja",
-                                       "haepi beolseudei mai beuro~~~","haepi beolseudei tu yu~~!","hwaeum jjeoreossji?","uri amu saenggak anhago","saengilppang!! aak!!")
-
-    // [BANGTAN BOMB] '풍경' Special LIVE - BTS (방탄소년단)       변수명 첫 시작을 숫자로 지을수없어서 A 추가함
-    val A4lKqFR_67RI = arrayOf("풍경(Scenery)","풍경(Scenery) 프로듀서님입니다","크으~~~","태형이형 이거 퓽경아냐 풍경?","맞아","별이 가득한 공기에","나나나 그대를 그려요","내안에 잠이 올까요","가사 까먹었어??","새벽 달이 지난 공원에","지금 내 감정을 담아요","이 노랜 그댈 향해요","밤하늘 달에게 비춰진","필름의 소리를 들어요","I still wonder wonder beautiful story","Still wonder wonder best part","I still wander wander next story",
-                         "I want to make you mine")
-    val A4lKqFR_67RI_time = arrayOf(1,4,17,22,23,24,30,32,35,39,43,46,55,59,69,75,82,88)
-    val A4lKqFR_67RI_sub = arrayOf("Scenery","He's a ‘풍경(Scenery)’producer","Cool~~~","Taehyung, isn't this a Scenery?","That’s right","in the air full of stars","Na na na~~ I draw you~","Will I fall asleep inside?","Did you forget the lyrics?","In the park that passed the early morning moon","I want you to put my feelings in it right now"
-                                ,"I'm heading for you, this song","reflected by the night sky moon","Listen to the sound of the film","I still wonder wonder beautiful story","Still wonder wonder best part","I still wander wander next story","I want to make you mine")
-    val A4lKqFR_67RI_diction = arrayOf("punggyeong","punggyeong peurodyuseonimipnida","keueu~~~","taehyeongihyeong igeo pyunggyeonganya punggyeong?","maja","byeori gadeukhan gonggie","nanana geudaereul geuryeoyo","naeane jami olkkayo","gasa kkameogeosseo??","saebyeok dari jinan gongwone","jigeum nae gamjeongeul damayo","i noraen geudael hyanghaeyo","bamhaneul darege bichwojin"
-                                ,"pilleumui sorireul deureoyo","I still wonder wonder beautiful story","Still wonder wonder best part","I still wander wander next story","I want to make you mine")
-
-    //[BANGTAN BOMB] V&Jungkook Singing at standby time - BTS (방탄소년단)
-    val A3_FXW0CW_8o = arrayOf("매일 울려 이 노래가","널 떠올리게 만들어","다 우리 얘기만 같아서","아무리 귀를 막아봐도","자꾸 맴돌아","또 매일 울려 이 노래가","널 떠올리게 만들어","다 우리 얘기만 같아서","아무리 귀를 막아봐도","자꾸 맴돌아","듣고 싶지 않아 몸부림 쳐도","매일 듣는 노래","그 노래말은 꼭 우리가 됐고","그렇게 남은건 남겨진 하루들","아주 작은방 홀로","조심스레 다시","널 불러본다~~","정국이 완전히 노래방왔네 지금 ㅋ")
-    val A3_FXW0CW_8o_time = arrayOf(1,5,7,11,17,20,23,26,32,36,39,45,49,54,60,68,70,77)
-    val A3_FXW0CW_8o_sub = arrayOf("It makes me cry every day","It makes me think of you","Because it sounds like our story","No matter how hard I try to cover my ears","It spins around me","It makes me cry every day","It makes me think of you","Because it sounds like our story","No matter how hard I try to cover my ears","It spins around me"
-                                ,"I don’t wanna hear it, I throw a fit","But I hear it every day","The lyrics used to sound like someone else’s story but it became ours","The only thing remaining are the ruined days","Playing this song in my small room","carefully again","I'm calling you","Jungkook is in karaoke now")
-    val A3_FXW0CW_8o_diction = arrayOf("chuhu eopdeiteu yejeongipnida ㅠㅠ jogeumman gidaryeojuseyo ")
-
-    val gqvEO6o1Mx8 = arrayOf("추후 업데이트 예정입니다 ㅠㅠ조금만 기다려주세요 ","으으음~~")
-    val gqvEO6o1Mx8_time = arrayOf(5)
-    val gqvEO6o1Mx8_sub = arrayOf("We're going to update later. ㅠㅠ Please wait a moment")
-    val gqvEO6o1Mx8_diction = arrayOf("chuhu eopdeiteu yejeongipnida ㅠㅠ jogeumman gidaryeojuseyo ")
-
-    val FsEGaURjZ8w = arrayOf("추후 업데이트 예정입니다 ㅠㅠ조금만 기다려주세요","으으음~~")
-    val FsEGaURjZ8w_time = arrayOf(5)
-    val FsEGaURjZ8w_sub = arrayOf("We're going to update later. ㅠㅠ Please wait a moment")
-    val FsEGaURjZ8w_diction = arrayOf("chuhu eopdeiteu yejeongipnida ㅠㅠ jogeumman gidaryeojuseyo ")
-
-    val fgja5tdRB8o = arrayOf("추후 업데이트 예정입니다 ㅠㅠ조금만 기다려주세요","으으음~~")
-    val fgja5tdRB8o_time = arrayOf(5)
-    val fgja5tdRB8o_sub = arrayOf("We're going to update later. ㅠㅠ Please wait a moment")
-    val fgja5tdRB8o_diction = arrayOf("chuhu eopdeiteu yejeongipnida ㅠㅠ jogeumman gidaryeojuseyo ")
 
     //작은 것들을 위한 시
      val XsX3ATc3FbA_time = arrayOf(34, 37, 42, 46, 49, 52, 58, 61, 63, 65, 67, 71, 73, 76,80,83,85,88,90,94,95,98,102,107,111,112,113,117,120,125,127,130,134,138,139,142,145,148,151,153,156,159,162,165,167,170,173,176,178,180,183,194,195,198,200,201,203,205,207,209,211,213,215,219,221,224,233,236,240)
@@ -202,5 +142,75 @@ class Subtitles {
     "than a moment, than a moment, love",
     "(Ooh ah ooh ah ooh ah ooh ah ah) love it nothing stronger",
     "(Ooh ah ooh ah ooh ah ooh ah ooh) (than a boy with) than a boy with love")
+
+    val XsX3ATc3FbA_sub = arrayOf("I'm curious about everything how's your day",
+        "Oh tell me (oh yeah yeah, ah yeh ah yeh)",
+        "I wonder what makes you happy",
+        "Oh text me (oh yeah yeah, ah yeh ah yeh)",
+        "Your every picture",
+        "I want to put it on my bedside",
+        "Come be my teacher",
+        "I need you to teach me everything",
+        "Your one, your two",
+        "Listen my my baby i'm",
+        "flying high in the sky",
+        "(with the two wings you gave me then)",
+        "It's too high here now",
+        "I want to fit you in my eyes",
+        "Yeah you makin' me a boy with love",
+        "Oh my my my, oh my my my",
+        "I've waited all my life",
+        "I want to be all of you",
+        "Oh my my my, oh my my my",
+        "Looking for something right",
+        "Now I know a little bit",
+        "love is nothing stronger",
+        "(Ooh ah ooh ah ooh ah ooh ah ah) love is nothing stronger",
+        "(Ooh ah ooh ah ooh ah ooh ah ooh)",
+        "than a boy with love",
+        "Since I knew you, ya, my life has been all over you",
+        "You're the one who made the little things not trivial",
+        "(Oh yeah) One to ten ay ay all is special ay ay",
+        "Your interest in the gait and",
+        "Even the little habits (oh ah)",
+        "Ay Everyone says I was too small to be a hero (oh nah)",
+        "I say fate wasn't mine from the first place (oh nah)",
+        "world peace (no way)",
+        "a great order (no way)",
+        "I'm just gonna protect you (boy with love)",
+        "Listen my my baby i'm",
+        "flying high in the sky",
+        "(with the two wings you gave me then)",
+        "It's too high here now",
+        "I want to fit you in my eyes",
+        "Yeah you makin' me a boy with love",
+        "Oh my my my, oh my my my",
+        "You got me high so fast",
+        "I want to be all of you",
+        "Oh my my my, oh my my my",
+        "You got me fly so fast",
+        "Now I know a little bit",
+        "than a boy with love",
+        "(Ooh ah ooh ah ooh ah ooh ah ah) love is nothing stronger",
+        "(Ooh ah ooh ah ooh ah ooh ah ooh) (than a boy with) than a boy with love",
+        "I'll be honest with you",
+        "I didn't know it was hard",
+        "Higher sky, larger hall",
+        "Sometimes I prayed to make him run away",
+        "But Your wound is my wound",
+        "When I realized, I made up my mind",
+        "With the wings of the Icarus you gave me",
+        "To you, not the sun",
+        "Let me fly",
+        "Oh my my my, oh my my my",
+        "I've waited all my life",
+        "I want to be all of you",
+        "Oh my my my, oh my my my",
+        "Looking for something right",
+        "Now I know a little bit",
+        "than a moment, than a moment, love",
+        "(Ooh ah ooh ah ooh ah ooh ah ah) love is nothing stronger",
+        "(Ooh ah ooh ah ooh ah ooh ah ooh) (than a boy with) than a boy with love")
+
 
 }
