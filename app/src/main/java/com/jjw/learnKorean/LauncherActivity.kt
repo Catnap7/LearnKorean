e0445a87435017c2522c566b5e6f0c53f0e17a39
JangJaeWoo <wkdwoduf333@naver.com>
2020-09-15 17:15:45 +0900

    splash 화면 추가

---
+++ app/src/main/java/com/jjw/learnKorean/LauncherActivity.kt
@@ -0,0 +1,26 @@
+package com.jjw.learnKorean
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class LauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+
+        loading()
+
+    }
+
+    private fun loading() {
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+            finish()
+        }, 1000)
+    }
+}
